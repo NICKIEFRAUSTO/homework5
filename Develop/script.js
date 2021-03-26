e2f3a5d7f9bced0bddababcb1c727b561ac0e83b
Nickie Frausto <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-03-25 23:22:54 -0500

    added an array for the timeblocks and a function for changing the colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,38 @@
-let now = moment().hour();
-
-let nMoment = moment().format('MMMM Do YYYY, h:mm:ss a');
-let display = $('#currentDay');
-
-display.text(nMoment);
+let now = moment().format('MMMM Do YYYY, h:mm:ss a');
+let dotw = moment().format('dddd');
+let displayday = $('#dayName');
 
 
+function displayTime() {
+    let now = moment().format("MMM Do, YYYY h:mm:ss a");
+    $("#currentDay").text(now);
+    displayday.text(dotw);
+    
+}
+setInterval (displayTime, 1000);
 
+
+let timeBlockEls = [
+    { AM9: "09:00PM"},
+    { AM10: "10:00"},
+    { AM11: "11:00"},
+    { PM12: "12:00"},
+    { PM1:  "01:00"},
+    { PM2:  "02:00"},
+    { PM3:  "03:00"},
+    { PM4:  "04:00"},
+    { PM5:  "05:00"}
+]
+
+for (var i = 0; i < timeBlockEls.length; i++) {
+    function colorChange() {
+      if (timeBlockEls<moment().hour())  {
+          timeBlockEls.addclass('.future')
+      }
+    }if (timeBlockEls>moment().hour()) {
+        timeBlockEls.attr('.past')
+
+    
+    }
+}
 
